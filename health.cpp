64798c773f4bd3ec506e4a1d0e408a166f5cc5f9
Marcus Robertsson <marcus.asberg.98@gmail.com>
2024-08-13 18:23:06 +0200

    Have to run, started making the display health function

--- health.cpp
+++ health.cpp
@@ -2,8 +2,22 @@
 
 Health::Health()
 {
+
+    CurrentHealth = 100f;
 }
 
 Health::~Health()
+{
+}
+
+void Health::Damage(bool hit)
+{
+    if (hit)
+    {
+        CurrentHealth -= 20f;
+    }
+}
+
+void Health::DisplayHealth(void)
 {
 }
--- health.h
+++ health.h
@@ -1,12 +1,20 @@
 #ifndef HEALTH_H
 #define HEALTH_H
 
+#include "raylib.h"
+
 class Health
 {
 private:
 public:
     Health();
     ~Health();
+
+    float CurrentHealth;
+
+    void Damage(bool hit);
+
+    void DisplayHealth(void);
 };
 
 #endif
