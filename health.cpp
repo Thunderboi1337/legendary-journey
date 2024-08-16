8c3356c569cc6fc158ac43cf5e1ef0e0a0108012
Marcus Robertsson <marcus.asberg.98@gmail.com>
2024-08-16 09:26:33 +0200

    cooking

--- health.cpp
+++ health.cpp
@@ -2,14 +2,63 @@
 
 Health::Health()
 {
+    sprite = LoadTexture("heart.png");
 
     CurrentHealth = 100.f;
     GameOver = false;
     Dead = false;
+
+    health_level = FIVE_HEART;
+
+    framesSpeed = 5;
+    currentFrame = 0;
+    framesCounter = 0;
+    frameCounter = 0;
+
+    frameRec = {0.0f, 0.0f, (float)sprite.width / 4, (float)sprite.height / 3};
 }
 
 Health::~Health()
 {
+    UnloadTexture(sprite);
+}
+
+void Health::DisplayHealth(void)
+{
+
+    frameCounter++;
+
+    // Update the current frame based on the frame speed
+    if (frameCounter >= (60 / framesSpeed))
+    {
+        frameCounter = 0;
+        currentFrame++;
+
+        // Loop back the frame when reaching the end
+        if (currentFrame > 4)
+            currentFrame = 0;
+
+        // Set the frameRec for the current frame
+        frameRec.x = (float)currentFrame * (float)sprite.width / 4;
+    }
+
+    switch (health_level)
+    {
+    case FIVE_HEART:
+        for (int i = 0; i < 5; i++)
+        {
+            DrawTexture(sprite, 10 * i, 10, WHITE);
+        }
+        break;
+
+    case FOUR_HEART:
+        break;
+
+    default:
+        break;
+    }
+
+    DrawRectangle(10, 10, CurrentHealth, 20, GREEN);
 }
 
 void Health::Damage(bool hit)
@@ -22,9 +71,4 @@ void Health::Damage(bool hit)
             Dead = true;
         }
     }
-}
-
-void Health::DisplayHealth(void)
-{
-    DrawRectangle(10, 10, CurrentHealth, 20, GREEN);
 }
--- health.h
+++ health.h
@@ -3,6 +3,15 @@
 
 #include "raylib.h"
 
+enum HealthLevel
+{
+    ONE_HEART = 20,
+    TWO_HEART = 40,
+    THREE_HEART = 60,
+    FOUR_HEART = 80,
+    FIVE_HEART = 100
+}
+
 class Health
 {
 private:
@@ -14,9 +23,19 @@ public:
     bool GameOver;
     bool Dead;
 
-    void Damage(bool hit);
+    HealthLevel health_level;
+
+    Texture2D sprite;
+
+    Rectangle frameRec;
+    int currentFrame;
+    int framesCounter;
+    int framesSpeed;
+    int frameCounter;
+    bool facingRight;
 
     void DisplayHealth(void);
+    void Damage(bool hit);
 };
 
 #endif
